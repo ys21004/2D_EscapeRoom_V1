fabac85c4e081a6ebcbe213ad3a7cf9d3effaf43
ys21004 <YS2100433@tkh.edu.eg>
2024-04-30 02:33:49 +0300

    New Game Mechanics + Inventory
    Changed the way rooms are viewed. Instead of having each room in an individual scene, I added a camera for each room and attached a script that allows the player to view each room. Also started implementing the player inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slots;
+    public bool[] isSelected;
+
+    private void Start()
+    {
+        isSelected = new bool[6];
+        for (int i = 0; i < isSelected.Length; i++)
+        {
+            isSelected[i] = false;
+            isFull[i] = false;
+        }
+    }
+}
